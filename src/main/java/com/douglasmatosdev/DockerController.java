8bbd746081d20884cef32b68db1db8bd5ecc69f7
dougsoftware <douglasmatosdev@gmail.com>
2025-05-31 09:35:29 -0300

    Initial commit

---
+++ src/main/java/com/douglasmatosdev/DockerController.java
@@ -0,0 +1,20 @@
+package com.douglasmatosdev;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+	
+	@RequestMapping("/hello-docker")
+	public HelloDocker greeting() {
+		
+		//var hostName = System.getenv("COMPUTERNAME");
+		var hostName = System.getenv("HOSTNAME");
+		
+		return new HelloDocker(
+					"Hello Docker",
+					hostName
+				);
+	}
+}
